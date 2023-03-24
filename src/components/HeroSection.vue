da1fd088b20cfba8fdb84c8ff46e773510029c56
simoneeth <bucciolsimone@gmail.com>
2023-03-24 21:50:48 +0100

    chore: hero page

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,14 +1,26 @@
 <script>
-import { defineComponent, computed } from "vue";
+import { defineComponent,ref, computed } from "vue";
 import {useBreakpoint} from "../functions/useBreakpoint"
+import Logo from "../assets/svg/barker-logo.svg"
+import Copy from "../assets/svg/copy-icon.svg"
+
 export default defineComponent({
   name: "HeroSection",
+  components: {Logo, Copy},
   setup(){
+    const contractAddress = ref('0xe924dd20c41bb088f1b8557ca8fc5de99d1b4da8')
+
     const matches = useBreakpoint()
     const isMobile = computed(() => matches.value?.beforeLg)
 
+    const copyContract = () => {
+      navigator.clipboard.writeText(contractAddress.value)
+    }
+
     return {
-      isMobile
+      isMobile,
+      contractAddress,
+      copyContract
     }
   }
 });
@@ -16,8 +28,105 @@ export default defineComponent({
 
 <template>
   <section class="hero-section h-screen w-full relative">
+    <nav class="w-full flex items-center justify-between">
+      <div class="logo-container flex items-center">
+        <Logo class="logo" />
+        <h1 class="text-logo">Barker</h1>
+      </div>
+      <div class="icons-container flex items-center">
+        <div class="icon" v-if="!isMobile"></div>
+        <div class="icon" v-if="!isMobile"></div>
+        <div class="icon" v-if="!isMobile"></div>
+        <div class="icon" v-if="!isMobile"></div>
+        <button v-if="isMobile">Menu</button>
+      </div>
+    </nav>
+    <div class="hero-content w-full">
+      <div v-if="isMobile" class="mobile-content w-full h-full relative">
+        Mobile
+        <div class="wave-wrapper absolute bottom-0 w-full flex items-center justify-center">
+          <div class="contract-wrapper flex items-center">
+            <p class="contract-label mr-4">{{contractAddress}}</p>
+            <Copy class="copy-logo" @click="copyContract()" />
+          </div>
+        </div>
+      </div>
+      <div v-else class="desktop-content w-full h-full relative">
+        Desktop
+        <div class="wave-wrapper absolute bottom-0 w-full flex items-center justify-center">
+          <div class="contract-wrapper flex items-center">
+            <p class="contract-label mr-4">{{contractAddress}}</p>
+            <Copy class="copy-logo" @click="copyContract()" />
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <style scoped lang="scss">
+.logo-container {
+  width: fit-content;
+}
+nav {
+  padding-left: rem(15);
+  padding-right: rem(15);
+  padding-top: rem(20);
+  @screen lg {
+    padding-left: rem(100);
+  padding-right: rem(100);
+  padding-top: rem(30);
+  }
+}
+
+.logo{
+  height: rem(60);
+  margin-right: rem(15);
+}
+
+.text-logo{
+  font-family: Black;
+  font-size: rem(36);
+  color: $c-blue;
+}
+
+.icon {
+  background-color: $c-blue;
+  height: rem(50);
+  width: rem(50);
+  border-radius: rem(100);
+}
+
+.icons-container .icon:not(:last-child){
+  margin-right: rem(16);
+}
+
+.hero-content{
+  height: calc(100% - 80px);
+
+  @screen lg {
+    height: calc(100% - 90px);
+  }
+}
+
+.wave-wrapper{
+  height: rem(75);
+  background: url('../assets/images/hero-wave.png');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.contract-label{
+  font-family: "regular";
+  color: white;
+  font-size: rem(12);
+  @screen lg {
+    font-size: rem(18);
+  }
+}
+.copy-logo{
+  height: rem(25);
+  cursor: pointer;
+}
 </style>
