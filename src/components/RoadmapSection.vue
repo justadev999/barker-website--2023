b34365f928e6f9e8d89eca588abbb8a43a3ecfb4
simoneeth <bucciolsimone@gmail.com>
2023-03-26 14:35:12 +0200

    test

--- src/components/RoadmapSection.vue
+++ src/components/RoadmapSection.vue
@@ -1,12 +1,10 @@
 <script>
 import { defineComponent, computed} from 'vue'
-import MobRoadmap from "../assets/svg/mob-roadmap.svg"
-import DeskRoadmap from "../assets/svg/desk-roadmap.svg"
 import { useBreakpoint } from '../functions/useBreakpoint'
 
 export default defineComponent({
 name: 'RoadmapSection' ,
-components: {MobRoadmap, DeskRoadmap},
+// components: {MobRoadmap, DeskRoadmap},
 setup () {
     const matches = useBreakpoint()
     const isMobile = computed(() => matches.value?.beforeLg)
@@ -19,7 +17,6 @@ setup () {
 
 <template>
     <section class="roadmap-section h-screen w-full lg:px-4 py-4">
-        <MobRoadmap v-if="isMobile" class="w-full h-full" />
-        <DeskRoadmap v-if="!isMobile" class="w-full h-full" />
+        test
     </section>
 </template>
